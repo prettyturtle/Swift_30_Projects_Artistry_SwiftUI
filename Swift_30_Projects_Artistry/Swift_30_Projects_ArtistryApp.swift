c3d18658bea3df92f3191ae07b8398c488671f45
prettyturtle <unifconti@gmail.com>
2023-05-06 19:10:06 +0900

    Initial Commit

---
+++ Swift_30_Projects_Artistry/Swift_30_Projects_ArtistryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_ArtistryApp.swift
+//  Swift_30_Projects_Artistry
+//
+//  Created by yc on 2023/05/06.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_ArtistryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
